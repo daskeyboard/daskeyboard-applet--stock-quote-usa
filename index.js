d1ee0561523dc7a982e52e12b86b4c9a67bb5d82
fabiendv <dallavalle.fabien@gmail.com>
2026-02-06 14:42:44 +0100

    fix: upgrade Yahoo Finance integration to 3.13.0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const q = require('daskeyboard-applet');
-const yahooFinance = require('yahoo-finance2').default;
+const YahooFinance = require('yahoo-finance2').default;
+const yahooFinance = new YahooFinance();
 const logger = q.logger;
 
 function round(number) {
